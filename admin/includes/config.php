c2615458149061bc1da31568ad0e3c43e00c8bdc
AakarshitChaurasia <71507490+AakarshitChaurasia@users.noreply.github.com>
2022-09-04 14:21:53 +0530

    Update config.php

--- admin/includes/config.php
+++ admin/includes/config.php
@@ -1,7 +1,7 @@
 <?php
-define('DB_SERVER','localhost');
-define('DB_USER','root');
-define('DB_PASS' ,'');
+define('DB_SERVER','webblog.mysql.database.azure.com');
+define('DB_USER','adminblog');
+define('DB_PASS' ,'@Aakarshit132@');
 define('DB_NAME','bmsdb');
 $con = mysqli_connect(DB_SERVER,DB_USER,DB_PASS,DB_NAME);
 // Check connection
